147aba3649de52b57e74f8f9067a19cd4c9a30eb
delusidiot <delusidiot@gmail.com>
2023-07-19 13:30:19 +0900

    feat: add Spring Security
    Jira
    [DP-1]

---
+++ back-end/AuthenticationService/src/main/java/com/drawhale/authenticationservice/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.drawhale.authenticationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final String[] PERMIT_URI = {"/users/**"};
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(PERMIT_URI).permitAll();
+                })
+                .sessionManagement(session-> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+        return http.build();
+    }
+}
